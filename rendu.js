04145d5b3442ad2403a6b6f4e17d51fcdea67411
averrani <aris.issad@gmail.com>
2023-02-17 10:45:11 +0100

    final commit

--- rendu.js
+++ rendu.js
@@ -1,24 +1,51 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync('users.json');
-//console.log(rawdata);
+//on a recupéré les données du fichier et on les stocke dans users
 let users = JSON.parse(rawdata);
 let country = new Array(users.length);
+let company = new Array(users.length);
 
-for(i = 0; i < users.length; i++){
-    country[i] = users[i].country;
+const input =process.argv[2];
+
+if (input === 'country') {
+    //on met que les country dans country
+    for (i = 0; i < users.length; i++) {
+        country[i] = users[i].country;
+    }
+    // on compte les country et on les met dans l'objet counts
+    const counts = {};
+    for (const num of country) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    let res = [];
+    // on transforme l'objet counts en tableau
+    for (i in counts) {
+        res.push({ "country": i, "count": counts[i] });
+    }
+
+    //on sort le tableau
+    res.sort((a, b) => b.count - a.count);
+    console.log(res);
+
+}else if(input === 'company'){
+    //on met que les company dans company
+    for (i = 0; i < users.length; i++) {
+        company[i] = users[i].company;
+    }
+    // on compte les company et on les met dans l'objet counts
+    const counts = {};
+    for (const num of company) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+
+    let res = [];
+    // on transforme l'objet counts en tableau
+    for (i in counts) {
+        res.push({ "company": i, "count": counts[i] });
+    }
+
+    //on sort le tableau
+    res.sort((a, b) => b.count - a.count);
+    console.log(res);
 }
-
-const counts = {};
-for (const num of country) {
-    counts[num] = counts[num] ? counts[num] + 1 : 1;
-}
-
-let res = []; 
-
-
-for(i in counts){
-    res.push({"country" : i, "count" : counts[i]});
-}
-
-res.sort((a,b) => b.res - a.res);
-console.log(res);
